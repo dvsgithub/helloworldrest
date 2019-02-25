2e906754f06ede2e4e6486ba3d756c1cb09d7136
DmitryShevchenko <shevchenko@soft-xpansion.com.ua>
2019-02-25 16:31:57 +0200

    Add new functionality

--- src/test/java/com/sx/dvs/helloworldrest/HelloworldrestApplicationTests.java
+++ src/test/java/com/sx/dvs/helloworldrest/HelloworldrestApplicationTests.java
@@ -5,12 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloworldrestApplicationTests {
 
     @Test
     public void contextLoads() {
+        GreetingController greetingController = new GreetingController();
+        assertThat(greetingController.greeting("Spring").getContent(), containsString("Hello, Spring!"));
     }
 
 }
